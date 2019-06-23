253030889eabca40cf8e719dd2879e24892d5544
llatika <llatika@protonmail.com>
2019-06-23 14:12:32 +0200

    Drugi commit

--- app/src/main/java/com/example/sodziutkaaplikacja/DrawingInShop.java
+++ app/src/main/java/com/example/sodziutkaaplikacja/DrawingInShop.java
@@ -1,14 +1,81 @@
+
 package com.example.sodziutkaaplikacja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class DrawingInShop extends AppCompatActivity {
+
+
+    Button drawgrafics;
+    Button exit;
+    Button back;
+    Button generategrafics;
+    TextView drawgraf;
+    TextView discounts;
+
+    public void OpenFirstPage()
+    {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void OpenThirdPage()
+    {
+
+        Intent intent = new Intent(this, ListOfGrafics.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drawing_in_shop);
+        setContentView(R.layout.activity_list_of_grafics);
+
+
+        final Button generategrafics=(Button) findViewById(R.id.generategrafics);
+        final Button drawgrafics =(Button) findViewById(R.id.drawgrafics);
+        final Button exit =(Button) findViewById(R.id.exit);
+        final Button back=(Button) findViewById(R.id.back);
+        final TextView discounts =(TextView) findViewById(R.id.discounts);
+        final TextView drawgraf =(TextView) findViewById(R.id.drawgraf);
+
+        exit.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                OpenFirstPage();
+            }
+        });
+
+        generategrafics.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                OpenThirdPage();
+            }
+        }
+        );
+
+
+
+
     }
 }
+
+
+
--- app/src/main/java/com/example/sodziutkaaplikacja/MainActivity.java
+++ app/src/main/java/com/example/sodziutkaaplikacja/MainActivity.java
@@ -26,5 +26,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button gototheshopping =(Button) findViewById(R.id.gototheshopping);
+        final Button fantasy =(Button) findViewById(R.id.fantasy);
+        final Button specialocassion =(Button) findViewById(R.id.specialocassion);
+        final Button exit =(Button) findViewById(R.id.exit);
+        final TextView hello =(TextView) findViewById(R.id.hello);
+
+        gototheshopping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenSecondPage();
+            }
+        });
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+
     }
 }
